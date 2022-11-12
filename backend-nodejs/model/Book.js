6c0157bf3d25add9cb9bf672763ac328d4e4323f
Thanawat-git <buysai123gw@gmail.com>
2022-11-12 20:43:20 +0700

    create model and set db

---
+++ backend-nodejs/model/Book.js
@@ -0,0 +1,14 @@
+const mongoose = required('mongoose')
+const Schema = mongoose.Schema;
+
+let Book = new Schema({
+    name: {
+        type: String
+    },
+    price: { type: String },
+    description: { type: String },
+}, {
+    collection: 'books'
+})
+
+module.exports = mongoose.model("Book", Book)
